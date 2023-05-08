1814520c096468a75befcd601a131636c8eb093e
Brian Bissette <bbissette3@yahoo.com>
2023-05-08 13:55:28 -0500

    started backend routes

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-from flask import Flask, jsonify, request, make_response, render_template
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -23,6 +23,27 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 api=Api(app)
+
+class Pets(Resource):
+    def get(self):
+        pets = [pet.to_dict() for pet in Pet.query.all()]
+        return make_response(pets, 200)
+
+api.add_resource(Pets, '/pets')
+
+class Shelters(Resource):
+    def get(self):
+        shelters = [shelter.to_dict() for shelter in Shelter.query.all()]
+        return make_response(shelters, 200)
+    
+api.add_resource(Shelters, '/shelters')
+
+class Reviews(Resource):
+    def get(self):
+        reviews = [review.to_dict() for review in Review.query.all()]
+        return make_response(reviews, 200)
+    
+api.add_resource(Reviews, '/reviews')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
