3d910424ab75436f07968e59595a664f90c15ebc
Timothy <sanohoang@gmail.com>
2023-05-09 11:18:54 -0500

    started react app and added branches

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <h1>
+      Welcome <br />
+      Almighty Bicep <br />
+      Godlike Freddy <br />
+      Magnificent Timmy!
+    </h1>
+  );
+};
+
+export default Navbar;
