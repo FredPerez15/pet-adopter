b0463fd4d0140e7f14bea13bd94eed583a1cb1db
Brian Bissette <122562881+bbissette3@users.noreply.github.com>
2023-05-12 13:30:30 -0500

    Revert "config.py changes"

--- server/config.py
+++ server/config.py
@@ -9,23 +9,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-app = Flask(
-    __name__,
-    static_url_path='',
-    static_folder='../client/build',
-    template_folder='../client/build'
-    )
-
+app = Flask(__name__)
 app.secret_key = b'R\x83\ru\xf0\xe2\x92A\xad\x922Z\x19y\xc4n'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 
 bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
