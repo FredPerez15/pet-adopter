0882d695eab9bf80051ca217c2b1bd90cbea7423
Brian Bissette <bbissette3@yahoo.com>
2023-05-12 12:50:33 -0500

    bruhhhh

--- server/config.py
+++ server/config.py
@@ -9,7 +9,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='../client/build',
+    template_folder='../client/build'
+)
+
 app.secret_key = b'R\x83\ru\xf0\xe2\x92A\xad\x922Z\x19y\xc4n'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
